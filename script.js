72fb4c6ee0f9fec207b89c8cce735beacd68a456
Duda P. Brito <mariapaulabrito1807@gmail.com>
2025-11-29 15:50:45 -0300

    feat + fix: adiciona modo escuro e suporte a multi-idioma + transições de ui

--- script.js
+++ script.js
@@ -103,3 +103,28 @@ if (contactForm) {
     }, 1000);
   });
 }
+
+const themeToggleBtn = document.getElementById("theme-toggle");
+const themeIcon = themeToggleBtn.querySelector("i");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+  document.body.classList.add("dark-mode");
+  themeIcon.classList.remove("bi-moon-stars-fill");
+  themeIcon.classList.add("bi-sun-fill");
+}
+
+themeToggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    themeIcon.classList.remove("bi-moon-stars-fill");
+    themeIcon.classList.add("bi-sun-fill");
+    localStorage.setItem("theme", "dark");
+  } else {
+    themeIcon.classList.remove("bi-sun-fill");
+    themeIcon.classList.add("bi-moon-stars-fill");
+    localStorage.setItem("theme", "light");
+  }
+});
